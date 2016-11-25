3a04f0d83f34e4a2146723392bad024a57ef574f
Vai Wilmintje <Vai Wilmintje>
2016-11-25 16:02:48 +0700

    complete client services

--- Services/ClientServices.js
+++ Services/ClientServices.js
@@ -1,18 +1,46 @@
 "use strict";
 const express_1 = require('express');
 const Setting_1 = require('../Setting');
+const ClientController_1 = require('../Controllers/ClientController');
 const Auth_1 = require('../Auth');
 var ClientRouter = express_1.Router();
 var root = Setting_1.default('service');
 ClientRouter.get(root + 'client/get', Auth_1.default, (req, res) => {
+    ClientController_1.default.get(req.query.id).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 ClientRouter.get(root + 'client/getByDevice', Auth_1.default, (req, res) => {
+    var query = JSON.parse(req.query.query);
+    ClientController_1.default.getByDevice(req.query.model, req.query.serial).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 ClientRouter.get(root + 'client/getAll', Auth_1.default, (req, res) => {
+    var query = JSON.parse(req.query.query);
+    ClientController_1.default.getAll(query).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 ClientRouter.post(root + 'client/save', Auth_1.default, (req, res) => {
+    ClientController_1.default.save(req.body).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 ClientRouter.delete(root + 'client/delete', Auth_1.default, (req, res) => {
+    ClientController_1.default.delete(req.query.id).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ClientRouter;
--- Services/ClientServices.ts
+++ Services/ClientServices.ts
@@ -1,29 +1,51 @@
 ﻿import {Request, Response, Router} from 'express';
 import setting from '../Setting';
-import UserController from '../Controllers/ClientController';
+import ClientController from '../Controllers/ClientController';
 import auth from '../Auth';
 
 var ClientRouter = Router();
 var root = setting('service');
 
 ClientRouter.get(root + 'client/get', auth, (req: Request, res: Response) => {
-
+    ClientController.get(req.query.id).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 
 ClientRouter.get(root + 'client/getByDevice', auth, (req: Request, res: Response) => {
-
+    var query = JSON.parse(req.query.query);
+    ClientController.getByDevice(req.query.model, req.query.serial).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 
 ClientRouter.get(root + 'client/getAll', auth, (req: Request, res: Response) => {
-
+    var query = JSON.parse(req.query.query);
+    ClientController.getAll(query).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 
 ClientRouter.post(root + 'client/save', auth, (req: Request, res: Response) => {
-
+    ClientController.save(req.body).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 
 ClientRouter.delete(root + 'client/delete', auth, (req: Request, res: Response) => {
-
+    ClientController.delete(req.query.id).then(result => {
+        res.status(200).send(result);
+    }).catch(exception => {
+        res.status(500).send(exception.message);
+    });
 });
 
 export default ClientRouter;
